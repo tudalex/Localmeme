dd0071f66cefc4f803791bed3485211a8723c09d
Constantin A. Tudorica <tudalex@gmail.com>
2013-03-03 04:04:19 +0200

    Fixed a typo.

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ io.sockets.on('connection', function (socket) {
 			socket.emit('tag', item);
 		});
 		stream.on('end', function() {
-			socket.emti('tag_end', {});
+			socket.emit('tag_end', {});
 			console.log("End of tag stream.");
 		});
 	});
