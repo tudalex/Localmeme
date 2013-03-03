89f1287f83dcbdcc4e87b95d5d121135c8f96e4c
Gabriel Ivanica <ivanicagabriel@gmail.com>
2013-03-03 08:16:41 +0200

    asd

--- public/UI/socket.js
+++ public/UI/socket.js
@@ -39,59 +39,60 @@ socket.on('connect', function() {
     socket.emit('request_backgrounds');
     backgrounds_loaded = true;
   }
+  socket.on('news', function (data) {
+	  console.log(data);
+	});
+	
+	socket.on('meme', function (data) {
+		console.log(data);
+		Homepage.loadMeme(data);
+	});
+	
+	socket.on('tag', function (data){
+		//console.log(data);
+	  tags.push(data._id);
+	});
+	var tags_loaded = 0;
+	socket.on('tag_end', function(data) {
+	  var t = $("#tags_checkboxes");
+	  //console.log(t);
+	  t.empty();
+	  var legend = document.createElement('legend');
+	  legend.innerHTML = "Alegeti streamuri:";
+	  t.appendChild(legend);
+	  for (var id in tags) {
+	    var checkbox = document.createElement('input');
+	    checkbox.id = tags[id];
+	    checkbox.type = "checkbox";
+	    checkbox.setAttribute('name', '');
+	    var label = document.createElement('label');
+	    label.setAttribute('for', tags[id]); 
+	    label.innerHTML=tags[id];
+	    checkbox.addEventListener('change', function() {
+	      if (checkbox.value == true) 
+	        socket.emit('subscribe', {room: tags[id]});
+	      else
+	        socket.emit('unsubscribe', {room: tags[id]});
+	    });
+	    t.appendChild(checkbox);
+	    t.appendChild(label);
+	  }
+	  $("#tags").trigger("create");
+	
+	  //console.log(t);
+	
+	});
+	
+	socket.on('background', function (data) {
+	  backgrounds.push(data._id);
+	  //console.log(data);
+	});
+	
+	socket.on('background_end', function(data) {
+		Stats.backgrounds++;
+		if (Stats.backgrounds == 2)
+			BackgroundPage.loadImages();
+	});
 });
 
-socket.on('news', function (data) {
-  console.log(data);
-});
 
-socket.on('meme', function (data) {
-	console.log(data);
-	Homepage.loadMeme(data);
-});
-
-socket.on('tag', function (data){
-	//console.log(data);
-  tags.push(data._id);
-});
-var tags_loaded = 0;
-socket.on('tag_end', function(data) {
-  var t = $("#tags_checkboxes");
-  //console.log(t);
-  t.empty();
-  var legend = document.createElement('legend');
-  legend.innerHTML = "Alegeti streamuri:";
-  t.appendChild(legend);
-  for (var id in tags) {
-    var checkbox = document.createElement('input');
-    checkbox.id = tags[id];
-    checkbox.type = "checkbox";
-    checkbox.setAttribute('name', '');
-    var label = document.createElement('label');
-    label.setAttribute('for', tags[id]); 
-    label.innerHTML=tags[id];
-    checkbox.addEventListener('change', function() {
-      if (checkbox.value == true) 
-        socket.emit('subscribe', {room: tags[id]});
-      else
-        socket.emit('unsubscribe', {room: tags[id]});
-    });
-    t.appendChild(checkbox);
-    t.appendChild(label);
-  }
-  $("#tags").trigger("create");
-
-  //console.log(t);
-
-});
-
-socket.on('background', function (data) {
-  backgrounds.push(data._id);
-  //console.log(data);
-});
-
-socket.on('background_end', function(data) {
-	Stats.backgrounds++;
-	if (Stats.backgrounds == 2)
-		BackgroundPage.loadImages();
-});
